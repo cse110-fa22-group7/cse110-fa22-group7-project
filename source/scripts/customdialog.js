5f8dd2b405ae769b3474521d07f410ff0a96c72f
jAnke <jyenney@ucsd.edu>
2022-11-18 13:53:28 -0800

    ran prettier

--- source/scripts/customdialog.js
+++ source/scripts/customdialog.js
@@ -6,15 +6,18 @@ export function fill(message, isYesButton, isTextField) {
   // print out message to show
   const messageEl = dialogEl.querySelector("form > p");
   messageEl.textContent = message;
-  messageEl["style"] = "color: black; text-align: center; font: 2rem bold large;";
+  messageEl["style"] =
+    "color: black; text-align: center; font: 2rem bold large;";
 
   // dialog appear
-  (document.getElementById("output")).appendChild(dialogEl);
+  document.getElementById("output").appendChild(dialogEl);
 
   let yesButton = document.querySelector("#yes-button");
   let textField = document.querySelector("#prompt-message");
-  yesButton.style.display =(isYesButton)? "": yesButton.style.display = "none"; 
-  textField.style.display = (isTextField)? "" :"none";
+  yesButton.style.display = isYesButton
+    ? ""
+    : (yesButton.style.display = "none");
+  textField.style.display = isTextField ? "" : "none";
   // rendering to the screen
   displayDialog(message);
   return dialogEl;
@@ -25,7 +28,6 @@ export function closeDialog() {
   let dialog = document.querySelector("#output > #warning-dialog");
   dialog.close();
 }
-
 
 export function displayDialog(message) {
   let dialogEl = document.querySelector("#output > #warning-dialog");
