4dfb4ab2e490b5415a4b5406bddbd244fb547b49
bkim9 <bek001@ucsd.edu>
2022-11-25 16:39:07 -0800

    ran tests

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,50 +1,51 @@
 // collapse nav button
-const collapseBtn = document.querySelector("#collapse_button");
-const createBtn = document.querySelector('#create_button');
-const filterBtn = document.querySelector('.filter_by h2');
-const summaryDateEl = document.querySelector('summary#date_option');
-const summaryLabelEl = document.querySelector('summary#label_option');
+document.addEventListener("DOMContentLoaded", () => {
+  const collapseBtn = document.querySelector("#collapse_button");
+  const createBtn = document.querySelector("#create_button");
+  const filterBtn = document.querySelector(".filter_by h2");
+  const summaryDateEl = document.querySelector("summary#date_option");
+  const summaryLabelEl = document.querySelector("summary#label_option");
 
-const collapseContainer = document.querySelector('#collapse_container');
-const leftCreate = document.querySelector('.left_create');
-const filters= document.querySelector('.filters');
-const mainEl = document.querySelector('main');
-const filterImgEl = document.querySelector('img.filter_img');
+  const mainEl = document.querySelector("main");
+  const filterImgEl = document.querySelector("img.filter_img");
 
-let summaryDateStyle = summaryDateEl.style;
-let summaryLabelStyle = summaryLabelEl.style;
-let createBtnWidth = createBtn.style.width;
+  let summaryDateStyle = summaryDateEl.style;
+  let summaryLabelStyle = summaryLabelEl.style;
+  let createBtnWidth = createBtn.style.width;
 
-function expand() {
+  function expand() {
     collapseBtn.textContent = "<";
-    createBtn.innerHTML = '<img src="/source/images/create_white.svg" alt="create_image" height="22"> Create';
-    filterBtn.textContent = 'Filter by';
+    createBtn.innerHTML =
+      '<img src=\"/source/images/create_white.svg\" alt=\"create_image\" height=\"22\"> Create';
+    filterBtn.textContent = "Filter by";
     summaryDateEl.style = summaryDateStyle;
-    summaryLabelEl.style= summaryLabelStyle;
-    mainEl.style.gridTemplateColumns  =  '15rem auto';
+    summaryLabelEl.style = summaryLabelStyle;
+    mainEl.style.gridTemplateColumns = "15rem auto";
     createBtn.style.width = createBtnWidth;
-}
+  }
 
-function shrink() {
+  function shrink() {
     collapseBtn.textContent = ">";
-    createBtn.innerHTML = '<img src="/source/images/create_white.svg" alt="create_image" height="22">';
-    createBtn.style.width = 0
-    filterBtn.textContent = ''
-    summaryDateEl.style = 'display:none';
-    summaryLabelEl.style = 'display:none';
-    mainEl.style.gridTemplateColumns  =  '4.5rem auto';
-}
-collapseBtn.addEventListener("click", () => {
+    createBtn.innerHTML =
+      '<img src=\"/source/images/create_white.svg\" alt=\"create_image\" height=\"22\">';
+    createBtn.style.width = 0;
+    filterBtn.textContent = "";
+    summaryDateEl.style = "display:none";
+    summaryLabelEl.style = "display:none";
+    mainEl.style.gridTemplateColumns = "4.5rem auto";
+  }
+  collapseBtn.addEventListener("click", () => {
     // Collapse button textcontent to >
-    if( collapseBtn.textContent == '<') {
-        shrink();
+    if (collapseBtn.textContent == "<") {
+      shrink();
     } else {
-        expand();
+      expand();
     }
-});
+  });
 
-filterImgEl.addEventListener("click", () => {
-    if (collapseBtn.textContent == '>') {
-        expand();
+  filterImgEl.addEventListener("click", () => {
+    if (collapseBtn.textContent == ">") {
+      expand();
     }
-})
+  });
+});
